7588de55eca3f9004cd251b9a95c9ff24bcab4bb
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2018-06-03 13:30:29 +0100

    got add, edit, and delete working with local storage

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Recipe from './components/recipe.js';
 import AddRecipeModal from './components/add-recipe-modal.js';
 import DeleteRecipeModal from './components/delete-recipe-modal.js';
 import EditRecipeModal from './components/edit-recipe-modal.js';
-localStorage.clear()
+
 class App extends Component {
 
   constructor(props) {
@@ -109,7 +109,7 @@ class App extends Component {
         ingredients: this.state.addModal.ingredients
       })
       this.setState(tempState);
-      //localStorage.setItem('appData', this.state);
+      localStorage.setItem('appData', JSON.stringify(this.state));
     }
 
     // ===== EDIT RECIPE =====
@@ -140,7 +140,7 @@ class App extends Component {
       let tempState = this.state;
       tempState.recipes[this.state.editTargetIndex] = this.state.editModal;
       this.setState(tempState);
-      //localStorage.setItem('appData', this.state);
+      localStorage.setItem('appData', JSON.stringify(this.state));
     }
 
     // ===== DELETE RECIPE =====
@@ -156,7 +156,7 @@ class App extends Component {
       tempState.recipes.splice(this.state.deleteTargetIndex, 1);
       tempState.deleteTargetIndex = null;
       this.setState(tempState);
-      //localStorage.setItem('appData', this.state);
+      localStorage.setItem('appData', JSON.stringify(this.state));
     }
 
     // ===== =====
