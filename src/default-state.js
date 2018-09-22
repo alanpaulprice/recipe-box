450c084477c41fb7443300fe9be0a3cb146d91a2
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2018-09-22 21:23:57 +0100

    converted default state recipe ingredient strings to single lines

--- src/default-state.js
+++ src/default-state.js
@@ -14,43 +14,15 @@ const defaultState = () => {
       recipes: [
         {
           name: 'Pizza',
-          ingredients: `300g strong bread flour,
-          1 tsp instant yeast (from a sachet or a tub),
-          1 tsp salt,
-          1 tbsp olive oil (plus extra for drizzling),
-          100ml passata,
-          handful fresh basil or 1 tsp dried,
-          1 garlic clove, crushed,
-          125g ball mozzarella, sliced,
-          handful grated or shaved parmesan (or vegetarian alternative),
-          handful cherry tomatoes, halved,
-          handful basil leaves (optional)`
+          ingredients: '300g strong bread flour, 1 tsp instant yeast (from a sachet or a tub), 1 tsp salt, 1 tbsp olive oil (plus extra for drizzling), 100ml passata, handful fresh basil or 1 tsp dried, 1 garlic clove, crushed, 125g ball mozzarella, sliced, handful grated or shaved parmesan (or vegetarian alternative), handful cherry tomatoes, halved, handful basil leaves (optional)'
         },
         {
           name: 'Chicken Tikka Masala',
-          ingredients: `4 tbsp vegetable oil,
-          25g butter,
-          4 onions (roughly chopped),
-          6 tbsp chicken tikka masala paste,
-          2 red peppers, deseeded and cut into chunks,
-          8 boneless, skinless chicken breasts, cut into 2½ cm cubes,
-          2 x 400g cans chopped tomatoes,
-          4 tbsp tomato purée,
-          2-3 tbsp mango chutney,
-          150ml double cream,
-          150ml natural yogurt,
-          chopped coriander leaves, to serve`
+          ingredients: '4 tbsp vegetable oil, 25g butter, 4 onions (roughly chopped), 6 tbsp chicken tikka masala paste, 2 red peppers, deseeded and cut into chunks, 8 boneless, skinless chicken breasts, cut into 2½ cm cubes, 2 x 400g cans chopped tomatoes, 4 tbsp tomato purée, 2-3 tbsp mango chutney, 150ml double cream, 150ml natural yogurt, chopped coriander leaves, to serve'
         },
         {
           name: 'Chocolate Brownies',
-          ingredients: `185g unsalted butter,
-          185g best dark chocolate,
-          85g plain flour,
-          40g cocoa powder,
-          50g white chocolate,
-          50g milk chocolate,
-          3 large eggs,
-          275g golden caster sugar`
+          ingredients: '185g unsalted butter, 185g best dark chocolate, 85g plain flour, 40g cocoa powder, 50g white chocolate, 50g milk chocolate, 3 large eggs, 275g golden caster sugar'
         }
       ]
     }
